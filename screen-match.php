1022686caa7d5f6acbead880588b0f00330144ed
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-05-24 16:53:49 -0400

    evoluindo em php

--- screen-match.php
+++ screen-match.php
@@ -1,5 +1,19 @@
 <?php
 
+function exibeMensagemLancamento($ano) {
+    if ($ano > 2022) {
+        echo "O filme é Lançamento\n";
+    } elseif ($ano > 2020 && $ano <= 2022) {
+        echo "O filme ainda é novo\n";
+    } else {
+        echo "O filme não é Lançamento\n";
+    }
+}
+
+function incluidoNoPlano($planoPrime, $anoLancamento) {
+    return $planoPrime || $anoLancamento < 2020;
+}
+
 echo "Bem-vindo(a) ao screen match!\n";
 
 $nomeFilme = "Top Gun";
@@ -16,19 +30,13 @@ for ($contador = 1; $contador < $argc ; $contador++) {
 $notaFilme = array_sum($notas) / $quantidadeDeNotas;
 $planoPrime = true;
 
-$incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
+incluidoNoPlano($planoPrime, $anoLancamento);
 
 echo "Nome do filme: ".$nomeFilme."\n";
 echo "Nota do filme: ".$notaFilme."\n";
 echo "Ano de Lançamento:".$anoLancamento."\n";
 
-if ($anoLancamento > 2022) {
-    echo "O filme é Lançamento\n";
-} elseif ($anoLancamento > 2020 && $anoLancamento <= 2022) {
-    echo "O filme ainda é novo\n";
-} else {
-    echo "O filme não é Lançamento\n";
-}
+exibeMensagemLancamento($anoLancamento);
 
 $genero = match ($nomeFilme) {
     "Top Gun" => "Ação",
