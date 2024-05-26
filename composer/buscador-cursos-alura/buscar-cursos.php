9de16216999d5d68030826535cb6aba1e1b87fdf
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-05-26 18:21:16 -0400

    evoluindo no curso de composer

---
+++ composer/buscador-cursos-alura/buscar-cursos.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'vendor/autoload.php';
+require_once 'src/Buscador.php';
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['base_uri' => 'https://cursos.alura.com.br/']);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/category/programacao/php');
+
+foreach ($cursos as $curso) {
+    echo $curso . PHP_EOL;
+}
--- composer/buscador-cursos-alura/vendor/composer/autoload_real.php
+++ composer/buscador-cursos-alura/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit43e1033dc8a9e8245ee4bbce198283fd
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit43e1033dc8a9e8245ee4bbce198283fd', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit43e1033dc8a9e8245ee4bbce198283fd', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit43e1033dc8a9e8245ee4bbce198283fd
         call_user_func(\Composer\Autoload\ComposerStaticInit43e1033dc8a9e8245ee4bbce198283fd::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit43e1033dc8a9e8245ee4bbce198283fd::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
