55638e7c2fb6f384e3f346a7003d2533c3943446
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-05-25 22:43:24 -0400

    evoluindo muito em php orientad a objetos

---
+++ projeto-conta-banco/teste-saque.php
@@ -0,0 +1,20 @@
+<?php
+
+use Alura\Banco\Modelo\Conta\Conta;
+use Alura\Banco\Modelo\Conta\ContaCorrente;
+use Alura\Banco\Modelo\Conta\ContaPoupanca;
+use Alura\Banco\Modelo\Conta\Titular;
+use Alura\Banco\Modelo\CPF;
+use Alura\Banco\Modelo\Endereco;
+
+require_once "autoload.php";
+
+$conta = New ContaPoupanca(
+            new Titular(
+            new CPF('123.456.789-10'),
+            'Lucas Gabriel',
+            new Endereco('pvh', 'esp. comunidade', 'caula', '8101')));
+
+$conta->deposita(500);
+$conta->saca(100);
+echo $conta->recuperaSaldo();
