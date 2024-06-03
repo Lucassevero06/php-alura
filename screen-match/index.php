e87607b0f3d696d83447ee85fd8276b29f12d0ab
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-03 10:15:02 -0400

    evoluindo no php com POO

--- screen-match/index.php
+++ screen-match/index.php
@@ -7,9 +7,10 @@ require __DIR__ . "/src/Modelo/Filme.php";
 echo "Bem-vindo(a) ao ScreenMatch";
 
 $filme = new Filme();
-$filme->nome = 'Thor Ragnarok';
-$filme->anoLancamento = 2021;
-$filme->genero = "Super-Heroi";
+$filme->defineAnoLancamento(2021);
+//$filme->nome = 'Thor Ragnarok';
+//$filme->anoLancamento = 2021;
+//$filme->genero = "Super-Heroi";
 
 $filme->avalia(10);
 $filme->avalia(10);
@@ -18,4 +19,4 @@ $filme->avalia(5);
 
 var_dump($filme);
 
-echo $filme->media();
+echo $filme->media() . "/n";
--- screen-match/src/Modelo/Filme.php
+++ screen-match/src/Modelo/Filme.php
@@ -4,10 +4,10 @@ namespace Modelo;
 
 class Filme
 {
-    public string $nome;
-    public int $anoLancamento;
-    public string $genero;
-    public array $notas = [];
+    protected string $nome = "padrao";
+    protected int $anoLancamento = 2024;
+    protected string $genero = "padrao";
+    protected array $notas = [];
 
     public function avalia(float $nota): void
     {
@@ -20,6 +20,36 @@ class Filme
         $quantidadeNotas = count($this->notas);
 
         return $somaNotas / $quantidadeNotas;
+    }
+
+    public function anoLancamento(): int
+    {
+        return $this->anoLancamento;
+    }
+
+    public function defineAnoLancamento(int $anoLancamento): void
+    {
+        $this->anoLancamento = $anoLancamento;
+    }
+
+    public function nome(): int
+    {
+        return $this->nome;
+    }
+
+    public function defineNome(int $nome): void
+    {
+        $this->nome = $nome;
+    }
+
+    public function genero(): int
+    {
+        return $this->genero;
+    }
+
+    public function defineGenero(int $genero): void
+    {
+        $this->genero = $genero;
     }
 
 }
