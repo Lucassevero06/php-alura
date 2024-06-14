010e73a7080ae6236200e3ad860ddb108e21458b
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-14 09:15:48 -0400

    upt

---
+++ screen-match/erro.php
@@ -0,0 +1,31 @@
+<?php
+
+use ScreenMatch\Modelo\{
+    Filme, Episodio, Genero, Serie
+};
+
+use ScreenMatch\Calculos\{
+    CalculadoraDeMaratona, ConversorNotaEstrela
+};
+
+require 'autoload.php';
+
+$serie = new Serie(
+  'nome da serie',
+  2024,
+  Genero::Acao,
+  7,
+  20,
+  30
+);
+
+$episodio = new Episodio(
+    $serie,
+    'Piloto',
+    1
+);
+
+//$episodio->avalia(10);
+
+$conversor = new ConversorNotaEstrela();
+echo $conversor->converte($episodio);
