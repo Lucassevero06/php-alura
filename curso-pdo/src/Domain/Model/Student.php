00961c07081c0f819b1de199cb73f4818edfec1c
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-17 14:33:38 -0400

    evoluindo no projeto

--- curso-pdo/src/Domain/Model/Student.php
+++ curso-pdo/src/Domain/Model/Student.php
@@ -7,6 +7,8 @@ class Student //uma entidade
     private ?int $id;
     private string $name;
     private \DateTimeInterface $birthDate;
+    /** @var Phone[] */
+    private array $phones = [];
 
     public function __construct(?int $id, string $name, \DateTimeInterface $birthDate)
     {
@@ -49,5 +51,16 @@ class Student //uma entidade
         return $this->birthDate
             ->diff(new \DateTimeImmutable())
             ->y;
+    }
+
+    public function addPhone(Phone $phone): void
+    {
+        $this->phones[] = $phone;
+    }
+
+    /** @return Phone[] */
+    public function phones(): array
+    {
+        return $this->phones();
     }
 }
