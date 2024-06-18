484ee8492cde73bc1d5f75a2aa31917a94aa5917
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-18 14:21:55 -0400

    upt

---
+++ php-mysql/src/Modelo/Produto.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class Produto
+{
+    private string $id;
+    private string $tipo;
+    private string $nome;
+    private string $descricao;
+    private string $preco;
+
+}
--- php-mysql/src/conexao-bd.php
+++ php-mysql/src/conexao-bd.php
@@ -1,4 +1,3 @@
 <?php
 
 $pdo = new PDO('mysql:host=localhost;dbname=serenatto', 'lucas', 'Leonardo24#');
-var_dump($pdo);
