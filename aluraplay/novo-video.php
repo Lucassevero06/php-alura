fcd9be44ba3e2a8c549b610581c697c16227e5b1
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-19 11:57:36 -0400

    bglh tá ficando loco

--- aluraplay/novo-video.php
+++ aluraplay/novo-video.php
@@ -8,5 +8,9 @@ $statement = $pdo->prepare($sql);
 $statement->bindValue(1, $_POST['url']);
 $statement->bindValue(2, $_POST['titulo']);
 
+if ($statement->execute() === false) {
+    header('Location: /index.php?sucesso=0'); //redirecionamento de page
+} else {
+    header('Location: /index.php?sucesso=1');
+}
 
-var_dump($statement->execute());
