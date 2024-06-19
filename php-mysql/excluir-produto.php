8768d0262aec6fb3be4f25b4fc6a120ab1840db8
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-19 08:15:30 -0400

    funcionabilidades de criar, deletar e listar já funcionando

--- php-mysql/excluir-produto.php
+++ php-mysql/excluir-produto.php
@@ -1,7 +1,10 @@
 <?php
+
 require_once 'src/conexao-bd.php';
 require_once 'src/Modelo/Produto.php';
 require_once 'src/Repositorio/ProdutoRepositorio.php';
 
 $produtoRepositorio = new ProdutoRepositorio($pdo);
-$produtoRepositorio->deletar($_GET['id']);
+$produtoRepositorio->deletar($_POST['id']);
+
+header("Location: admin.php");  //redirecioname
--- php-mysql/src/Modelo/Produto.php
+++ php-mysql/src/Modelo/Produto.php
@@ -3,15 +3,14 @@
 
 class Produto
 {
-    private string $id;
+    private ?int $id;
     private string $tipo;
     private string $nome;
     private string $descricao;
-    private string $imagem;
-    private float $preco;
+    private ?string $imagem;
+    private string $preco;
 
-    public function __construct(string $id, string $tipo, string $nome, string $descricao, string $imagem, float $preco)
-    {
+    public function __construct(?int $id, string $tipo, string $nome, string $descricao, string $preco, string $imagem = "logo-serenatto.png")    {
         $this->id = $id;
         $this->tipo = $tipo;
         $this->nome = $nome;
@@ -45,7 +44,7 @@ class Produto
         return "img/" . $this->imagem;
     }
 
-    public function getPreco(): float
+    public function getPreco(): string
     {
         return $this->preco;
     }
