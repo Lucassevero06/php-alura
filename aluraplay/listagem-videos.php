3a047014429ef30de0d78f89f402f71190bb8062
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-20 08:50:13 -0400

    melhorando codigo com orientação a objetos em php

--- aluraplay/listagem-videos.php
+++ aluraplay/listagem-videos.php
@@ -1,26 +1,30 @@
 <?php
 
+use Alura\Mvc\Repository\VideoRepository;
+
 $dbPath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO("sqlite:$dbPath");
-$videoList = $pdo->query('SELECT * FROM videos;')->fetchAll(\PDO::FETCH_ASSOC);
+$repository = new VideoRepository($pdo);
+$videoList = $repository->all();
 
 ?>
 <?php require_once 'inicio-html.php'; ?>
     <ul class="videos__container">
         <?php foreach ($videoList as $video): ?>
-        <li class="videos__item">
-            <iframe width="100%" height="72%" src="<?= $video['url']; ?>"
-                title="YouTube video player" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen></iframe>
-            <div class="descricao-video">
-                <h3><?= $video['title']; ?></h3>
-                <div class="acoes-video">
-                    <a href="/editar-video?id=<?= $video['id']; ?>">Editar</a>
-                    <a href="/remover-video?id=<?= $video['id']; ?>">Excluir</a>
+            <li class="videos__item">
+                <iframe width="100%" height="72%" src="<?= $video->url; ?>"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <h3><?= $video->title; ?></h3>
+                    <div class="acoes-video">
+                        <a href="/editar-video?id=<?= $video->id; ?>">Editar</a>
+                        <a href="/remover-video?id=<?= $video->id; ?>">Excluir</a>
+                    </div>
                 </div>
-            </div>
-        </li>
+            </li>
         <?php endforeach; ?>
+
     </ul>
 <?php require_once 'fim-html.php'; ?>
--- aluraplay/src/Repository/VideoRepository.php
+++ aluraplay/src/Repository/VideoRepository.php
@@ -11,17 +11,58 @@ class VideoRepository
     {
     }
 
-    public function add(Video $video): Video
+    public function add(Video $video): bool
     {
         $sql = 'INSERT INTO videos (url, title) VALUES (?, ?)';
         $statement = $this->pdo->prepare($sql);
         $statement->bindValue(1, $video->url);
         $statement->bindValue(2, $video->title);
 
-        $statement->execute();
+        $result = $statement->execute();
         $id = $this->pdo->lastInsertId();
 
         $video->setId(intval($id));
-        return $video;
+        return $result;
+    }
+
+    public function remove(int $id): bool
+    {
+        $sql = 'DELETE FROM videos WHERE id = ?';
+        $statement = $this->pdo->prepare($sql);
+        $statement->bindValue(1, $id);
+
+        return $statement->execute();
+    }
+
+    public function update(Video $video): void
+    {
+        $sql = 'UPDATE videos SET url = :url, title = :title WHERE id = :id;';
+        $statement = $this->pdo->prepare($sql);
+
+        $statement->bindValue(':url', $video->url);
+        $statement->bindValue(':title', $video->title);
+        $statement->bindValue(':id', $video->id, PDO::PARAM_INT);
+
+        $statement->execute();
+    }
+
+
+    /**
+     * @return Video[]
+     */
+    public function all(): array
+    {
+        $videoList = $this->pdo
+            ->query('SELECT * FROM videos;')
+            ->fetchAll(PDO::FETCH_ASSOC);
+        return array_map(
+            function (array $videoData) {
+                $video = new Video($videoData['url'], $videoData['title']);
+                $video->setId($videoData['id']);
+
+                return $video;
+            },
+            $videoList
+        );
     }
 }
