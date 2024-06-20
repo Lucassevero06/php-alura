a8a7765abdb569d1206dfdf8cf3ffed1203470e7
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-20 13:49:09 -0400

    evoluindo no projeto

--- aluraplay/cria-usuario.php
+++ aluraplay/cria-usuario.php
@@ -3,8 +3,8 @@
 $dbPath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO("sqlite:$dbPath");
 
-$email = $argv;
-$password = $argv;
+$email = $argv[1];
+$password = $argv[2];
 $hash = password_hash($password, PASSWORD_ARGON2ID);
 
 $sql = 'INSERT INTO users (email, password) VALUES (?, ?);';
@@ -12,3 +12,4 @@ $statement = $pdo->prepare($sql);
 $statement->bindValue(1, $email);
 $statement->bindValue(2, $hash);
 $statement->execute();
+
