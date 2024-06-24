909aa91938de6aaa4e87a6430de400b3cb631a8b
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-24 08:37:39 -0400

    funcionabilidade de adicionar capa/imagem nos iframe

--- aluraplay/views/video-list.php
+++ aluraplay/views/video-list.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/inicio-html.php';
         <li class="videos__item">
             <?php if ($video->getFilePath() !== null): ?>
                 <a href="<?= $video->url; ?>">
-                    <img src="<?= $video->getFilePath(); ?>" alt="" />
+                    <img src="/img/uploads/<?= $video->getFilePath(); ?>" alt="" style="width: 100%" />
                 </a>
             <?php else: ?>
                 <iframe width="100%" height="72%" src="<?= $video->url; ?>"
