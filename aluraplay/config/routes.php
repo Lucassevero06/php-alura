450e49a808e248d67e49e51ab6929c3ccffcb976
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-06-24 10:44:02 -0400

    finalizando o alurapĺay

--- aluraplay/config/routes.php
+++ aluraplay/config/routes.php
@@ -2,9 +2,11 @@
 
 use Alura\Mvc\Controller\DeleteVideoController;
 use Alura\Mvc\Controller\EditVideoController;
+use Alura\Mvc\Controller\JsonVideoListController;
 use Alura\Mvc\Controller\LoginController;
 use Alura\Mvc\Controller\LoginFormController;
 use Alura\Mvc\Controller\LogoutController;
+use Alura\Mvc\Controller\NewJsonVideoController;
 use Alura\Mvc\Controller\NewVideoController;
 use Alura\Mvc\Controller\VideoFormController;
 use Alura\Mvc\Controller\VideoListController;
@@ -19,4 +21,6 @@ return [
     'GET|/login' => LoginFormController::class,
     'POST|/login' => LoginController::class,
     'GET|/logout' => LogoutController::class,
+    'GET|/videos-json' => JsonVideoListController::class,
+    'POST|/videos' => NewJsonVideoController::class,
 ];
