37f5dba149eef6a1408d0424f838ef52bce26848
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-07-18 10:38:49 -0400

    evoluindo com teste

--- PHP-e-TDD/leilao/tests/Service/AvaliadorTest.php
+++ PHP-e-TDD/leilao/tests/Service/AvaliadorTest.php
@@ -93,4 +93,28 @@ class AvaliadorTest extends TestCase
         // A verificação do resultado (Assert ou Then)
         self::assertEquals(2000, $menorValor); //verifica se dois valores são iguais
     }
+
+    public function testAvaliadorDeveBuscarOsTresMaioresLances()
+    {
+        $leilao = new Leilao('Fiat 147 0km');
+        $lucas = new Usuario('Lucas');
+        $joao = new Usuario('Joao');
+        $maria = new Usuario('Maria');
+        $jorge = new Usuario('Jorge');
+
+        $leilao->recebeLance(new Lance($lucas, 1500));
+        $leilao->recebeLance(new Lance($joao, 1000));
+        $leilao->recebeLance(new Lance($maria, 2000));
+        $leilao->recebeLance(new Lance($jorge, 1700));
+
+        $leiloeiro = new Avaliador();
+
+        $leiloeiro->avalia($leilao);
+
+        $maiores = $leiloeiro->getMaioresLances();
+        static::assertCount(3, $maiores);
+        static::assertEquals(2000, $maiores[0]->getValor());
+        static::assertEquals(1700, $maiores[1]->getValor());
+        static::assertEquals(1500, $maiores[2]->getValor());
+    }
 }
